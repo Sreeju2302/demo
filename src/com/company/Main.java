7259ea64bc0e26c289798ffdba1d86ec800b0523
Sreejum2302 <sreejum7@gmail.com>
2021-06-08 22:17:28 +0530

    first Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        boolean bool =  false;
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
-        System.out.println(print(word));
+        if (word!=null) {
+            System.out.println(print(word));
+            bool = true;
+        }  
+        Status status = new Status();
+        status.CheckStatus(bool);
+
     }
     private static  String print(String word){
         return word;
     }
 }
+
+class Status{
+    String success ="Success";
+    String fail = "Unsuccessful";
+
+    public String CheckStatus(boolean bool){
+        if(bool){
+            return success;
+        }
+        return fail;
+        }
+
+    }
+
