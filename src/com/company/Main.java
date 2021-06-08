a46ee20bd2baf7c4ea0f8270589e43e63b27eecf
Sreejum2302 <sreejum7@gmail.com>
2021-06-08 20:14:43 +0530

    first Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(print(word));
+    }
+    private static  String print(String word){
+        return word;
+    }
+}
